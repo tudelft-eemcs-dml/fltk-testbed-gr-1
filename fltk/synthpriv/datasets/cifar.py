0c4d879d75a545b7f09a03b085caa2a2c8413e57
Jan Buijnsters <buijnstersjan@gmail.com>
2021-05-24 22:54:50 +0200

    train_data is still empty for Cifar

--- fltk/synthpriv/datasets/cifar.py
+++ fltk/synthpriv/datasets/cifar.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
@@ -6,10 +7,18 @@ from torchvision import transforms
 from fltk.util.arguments import Arguments
 import logging
 
-from fltk.synthpriv.datasets.cifar100 import get_tuple_from_data_loader
 from fltk.datasets.cifar100 import CIFAR100Dataset
 from .base import BaseDistDataset, DataframeDataset
 
+# def get_tuple_from_data_loader(data_loader):
+#     """
+#     Get a tuple representation of the data stored in a data loader.
+#
+#     :param data_loader: data loader to get data from
+#     :type data_loader: torch.utils.data.DataLoader
+#     :return: tuple
+#     """
+#     return (next(iter(data_loader))[0].numpy(), next(iter(data_loader))[1].numpy())
 
 def load_cifar_train():
     # TODO:: Generate cifar100.txt
@@ -19,32 +28,51 @@ def load_cifar_train():
     test_data = cifar.load_test_dataset()
 
     # train_df, test_df, train_labels, test_labels = train_test_split(feats, labels, test_size=1 / 3, random_state=42)
-    return train_data
+
+    print(type(train_data))
+    print(train_data)
+    print("Tuple size")
+    print(len(train_data))
+    print(train_data[0].shape)
+    train_df = pd.DataFrame(train_data[0])
+    train_labels = train_data[1]
+
+    return (train_df, train_labels)
 
 def load_cifar_test():
     # TODO:: Generate cifar100.txt
     args = Arguments(logging)
     cifar = CIFAR100Dataset(args)
     test_data = cifar.load_test_dataset()
+    test_df = pd.DataFrame(test_data[0])
+    test_labels = test_data[1]
+    return (test_df, test_labels)
 
     # train_df, test_df, train_labels, test_labels = train_test_split(feats, labels, test_size=1 / 3, random_state=42)
-    return test_data
 
 
 class DistCifarDataset(BaseDistDataset):
     def __init__(self, args):
         args.get_logger().debug(f"Loading Cifar100")
+        cifarr = CIFAR100Dataset(args)
+        train_dataset = cifarr.get_train_dataset_loc()
+        test_dataset = cifarr.get_test_dataset_loc()
+        train_loader = DataLoader(train_dataset, batch_size=len(train_dataset))
+        test_loader = DataLoader(test_dataset, batch_size=len(test_dataset))
+
         super(DistCifarDataset, self).__init__(
             args=args,
             name=f"Cifar100",
-            train_datset=load_cifar_train(),
-            test_dataset=load_cifar_test(),
+            train_datset=train_dataset,
+            test_dataset=test_dataset,
+
         )
 
 
 if __name__ == "__main__":
-    train_df, train_labels, test_df, test_labels = load_cifar()
+    train_df = load_cifar_train()
+    test_df = load_cifar_test()
     print(train_df)
-    print(train_labels)
+    # print(train_labels)
     print(test_df)
-    print(test_labels)
+    # print(test_labels)
