dcca3ea2eac7d480ef1ec7c34154d4bf94dbe9e7
Jan Buijnsters <buijnstersjan@gmail.com>
2021-06-07 20:49:39 +0200

    Fix tensor shape

--- fltk/synthpriv/membershipWhiteboxAttacks/utils/eval.py
+++ fltk/synthpriv/membershipWhiteboxAttacks/utils/eval.py
@@ -13,7 +13,7 @@ def accuracy(output, target, topk=(1,)):
 
     res = []
     for k in topk:
-        print(correct[:k].view(-1))
-        correct_k = correct[:k].view(-1).float().sum(0)
+        # print(correct[:k].view(-1))
+        correct_k = correct[:k].contiguous().view(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
